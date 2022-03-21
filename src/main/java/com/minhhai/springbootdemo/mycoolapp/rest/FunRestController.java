8fb427d42a2c774b12e407d529bfcbf6ec16e76a
Nguyen Minh Hai <minhhai19062001@gmail.com>
2022-03-22 01:37:20 +0700

    add auto reload app

--- src/main/java/com/minhhai/springbootdemo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/minhhai/springbootdemo/mycoolapp/rest/FunRestController.java
@@ -7,8 +7,24 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+
     @GetMapping("/")
     public String showTime(){
         return "Hello World from Spring Boot. The current time is " + LocalDateTime.now();
+    }
+
+    @GetMapping("/workout")
+    public String workOut(){
+        return "Run a hard 5k";
+    }
+
+    @GetMapping("/fortune")
+    public String getFortune(){
+        return "Today is your lucky day";
+    }
+
+    @GetMapping("/high")
+    public String getHighScore(){
+        return "Today you'll get a high score";
     }
 }
