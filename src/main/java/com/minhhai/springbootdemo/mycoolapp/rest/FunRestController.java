6278abb46c3ded4558f9d00bac2c97b40f4b076c
Nguyen Minh Hai <minhhai19062001@gmail.com>
2022-03-23 01:38:45 +0700

    add support for application.properties

--- src/main/java/com/minhhai/springbootdemo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/minhhai/springbootdemo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.minhhai.springbootdemo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,15 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " + coachName + " is in " + teamName + " team";
+    }
 
     @GetMapping("/")
     public String showTime(){
